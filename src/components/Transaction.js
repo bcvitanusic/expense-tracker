d079553a5894c58a4226fd98ecfee018394e6bb2
insajddddd <branimir.cvitanusic@hotmail.com>
2020-05-04 21:00:56 +0200

    updated

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -12,7 +12,7 @@ export const Transaction = ({ transaction }) => {
           className="delete-btn"
           onClick={() => deleteTransaction(transaction.id)}
         >
-          x
+          &#10062;
         </button>
         {sign}
         {Math.abs(transaction.amount)}kn
